76f38ac75898eca16cdd06512f508614b7fc802e
samay <samayverma44@gmail.com>
2025-08-19 12:15:22 +0530

    fix: correct cookie name and enhance user management routes (find user, delete user, update user details)

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
 router.post('/signup', authController.signupUser);
 router.post('/login', authController.loginUser);    
 router.get('/logout', authController.logoutUser);
+
+// by Samay2006
+// @route   PATCH /user/:id
+router.get("/user/:id",authController.getuserbyid);
+router.patch('/update/:id',authController.updateuser);
+router.delete("/delete/:id",authController.deleteuser);
 
 module.exports = router;
