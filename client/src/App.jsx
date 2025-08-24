410468c1fda17830b1f0cbf5123dda5f510f5c69
nishu <nishurajput@gmail.com>
2025-08-24 14:30:47 +0530

    feat:added Navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import AuthPage from './pages/AuthPage'
-
+import Navbar from './components/Navbar';
 const App = () => {
   return (
+    <>
+    <Navbar/>
     <AuthPage/>
+   
+    </>
+    
   )
 }
 
