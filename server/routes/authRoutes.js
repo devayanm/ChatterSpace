0a32667e7b75f0da58472961e6234b6e446dc019
Vivek Kumar <vivekbxr30@gmail.com>
2025-08-26 21:20:00 +0530

    Created endpoints /forget-password and /reset-password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,6 +5,12 @@ router.post('/signup', authController.signupUser);
 router.post('/login', authController.loginUser);    
 router.get('/logout', authController.logoutUser);
 
+// by vivek724464
+// @route POST /forgot-password
+// @route POST /reset-password/:token
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password/:token", authController.resetPassword);
+
 // by Samay2006
 // @route   PATCH /user/:id
 router.get("/user/:id",authController.getuserbyid);
